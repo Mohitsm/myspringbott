8992709251eb3db56bb49123322af17572da16cb
Mohit Kumar <Mohit Kumar@Simranmohit>
2024-06-10 19:07:30 +0530

    code

--- employee/src/main/java/com/employee/config/CorsConfig.java
+++ employee/src/main/java/com/employee/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	 public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
 	                .allowedOriginPatterns("http://localhost:5173")
+	                .allowedOriginPatterns("http://localhost:8081")
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                .allowedHeaders("Content-Type")
 	                .allowCredentials(true)
