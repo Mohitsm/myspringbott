1c332762fcfb72d838764af22e05358073870a63
Mohit Kumar <Mohit Kumar@Simranmohit>
2024-06-10 18:45:40 +0530

    code puss

--- employee/src/main/java/com/employee/config/CorsConfig.java
+++ employee/src/main/java/com/employee/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
 	 public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOriginPatterns("http://fronted-react.s3-website.ap-south-1.amazonaws.com/")
+	                .allowedOriginPatterns("http://localhost:5173")
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                .allowedHeaders("Content-Type")
 	                .allowCredentials(true)
