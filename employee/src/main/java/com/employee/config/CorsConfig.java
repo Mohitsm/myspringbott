350cba3443e58c72dd61ea6ea96b1cb6ba65a5e3
Mohit Kumar <Mohit Kumar@Simranmohit>
2024-06-10 19:10:24 +0530

    cd

--- employee/src/main/java/com/employee/config/CorsConfig.java
+++ employee/src/main/java/com/employee/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
 	 public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOriginPatterns("http://localhost:5173")
+//	                .allowedOriginPatterns("http://localhost:5173")
 	                .allowedOriginPatterns("http://localhost:8081")
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                .allowedHeaders("Content-Type")
